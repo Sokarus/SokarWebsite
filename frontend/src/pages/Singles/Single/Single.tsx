dec38946f725f1dd32d4157d3fcb1ac489e7c813
Sokar <ddk21@bk.ru>
2026-03-27 20:31:45 +0400

    Adding github workflow actions

--- frontend/src/pages/Singles/Single/Single.tsx
+++ frontend/src/pages/Singles/Single/Single.tsx
@@ -1,7 +1,6 @@
 import { useSearchParams, useNavigate } from 'react-router-dom';
 import { type FC } from 'react';
 import { Container } from '@/components';
-import { Text } from '@/sharedComponents';
 import { singles } from '../constants';
 import { Store } from './Store';
 import './Single.scss';
@@ -30,11 +29,10 @@ export const Single: FC = () => {
           <source src={single.url} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        {/* <Text text={name} /> */}
         <div className={'StoresWrapper'}>
-        {single.stores.map((store) => (
-          <Store key={store.name} name={store.name} url={store.url} icon={store.icon} />
-        ))}
+          {single.stores.map((store) => (
+            <Store key={store.name} name={store.name} url={store.url} icon={store.icon} />
+          ))}
         </div>
       </div>
     </Container>
