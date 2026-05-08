bea74d48656e45e6cfaeda270611a244c81ad404
Sokar <ddk21@bk.ru>
2026-05-08 21:10:05 +0400

    Adding Dzhogoy single

--- frontend/src/pages/Singles/constants.ts
+++ frontend/src/pages/Singles/constants.ts
@@ -2,6 +2,37 @@ import { type SingleProps } from './Single';
 
 export const singles: SingleProps[] = [
   {
+    url: 'https://storage.yandexcloud.net/sokar/sokar-website/singles/%D0%93%D0%B0%D1%80%D0%BF%D0%B0%D0%BD%D0%B8%20%D0%A1%D0%BE%D0%BB%D0%B8%D0%BD%D0%B3%D0%B0%20%26%20Sokar%20-%20Dzhogoy.mp4',
+    name: 'Dzhogoy',
+    stores: [
+      {
+        name: 'Spotify',
+        url: 'https://open.spotify.com/track/1pxSTyqGot9fKovuA522l4',
+        icon: 'https://storage.yandexcloud.net/sokar/sokar-website/stores/spotify.png',
+      },
+      {
+        name: 'iTunes',
+        url: 'https://music.apple.com/us/song/dzhogoy/6766202077',
+        icon: 'https://storage.yandexcloud.net/sokar/sokar-website/stores/itunes.png',
+      },
+      {
+        name: 'Amazon Music',
+        url: 'https://music.amazon.com/tracks/B0GZH75JLY',
+        icon: 'https://storage.yandexcloud.net/sokar/sokar-website/stores/amazon.png',
+      },
+      {
+        name: 'YouTube Music',
+        url: 'https://music.youtube.com/watch?v=wvH-E1-Q24A',
+        icon: 'https://storage.yandexcloud.net/sokar/sokar-website/stores/youtube.png',
+      },
+      {
+        name: 'Yandex Music',
+        url: 'https://music.yandex.uz/album/41901270',
+        icon: 'https://storage.yandexcloud.net/sokar/sokar-website/stores/yandex.png',
+      },
+    ],
+  },
+  {
     url: 'https://storage.yandexcloud.net/sokar/sokar-website/singles/sokar%20-%20%D1%8F%20%D0%B2%D0%B0%D1%81%20%D0%BB%D1%8E%D0%B1%D0%BB%D1%8E.mp4',
     name: 'Я вас люблю',
     stores: [
